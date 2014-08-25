427f970cea1db3a4b1082464fe284b3f251a1300
BrianLoughnane <BrianJLoughnane@gmail.com>
2014-08-25 18:01:01 -0400

    4

--- js/app.js
+++ js/app.js
@@ -2,19 +2,27 @@ $(document).ready(function(){
 
 	$("form").submit(function(evt) {
 		evt.preventDefault();
-		console.log('hi');
+		console.log('form submitted');
 
 		var input = $('#search').val();
 
-		$.ajax({
-			url: 'http://developer.wordnik.com/v4/word.json/'+ input +'/definitions',
-			data: {
-				word: input,
-				api_key: 'dd938175caa9ac3a0a32c09fcc607d8638f0a0e82c8a0bbad',
+		$.ajax(
+			'http://developer.wordnik.com/v4/word.json/'+ input +'/definitions',
+			{
+				data: {
+					word: input,
+					limit: 200,
+					includeRelated: true,
+					useCanonical: false,
+					includeTags: false,
+					api_key: 'dd938175caa9ac3a0a32c09fcc607d8638f0a0e82c8a0bbad'
+				}, //end data
 				dataType: 'jsonp'
-			}
-		});
-
+			}, // end settings
+			function(r) {
+				console.log(r);
+			} //end callback
+		); //end ajax
 
 	});
 
