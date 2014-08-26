024476b5139d923bed489d62f5882acfbbd89829
BrianLoughnane <BrianJLoughnane@gmail.com>
2014-08-26 10:52:38 -0400

    7

--- js/app.js
+++ js/app.js
@@ -2,13 +2,12 @@ $(document).ready(function(){
 
 	$("form").submit(function(evt) {
 		evt.preventDefault();
-		console.log('form submitted');
 
 		$('h2').empty();
-		$('.examples').empty();
-		$('.center').empty().append('<h3>Related Words</h3>');
+		$('.pronounce').empty();
 		$('.left').empty().append('<h3>Definitions</h3>');
-
+		$('.center').empty().append('<h3>Related Words</h3>');
+		$('.right').empty().append('<h3>Examples</h3>');
 
 		var input = $('#search').val();
 
@@ -24,15 +23,30 @@ $(document).ready(function(){
 					api_key: 'dd938175caa9ac3a0a32c09fcc607d8638f0a0e82c8a0bbad'
 				}, //end data
 				success:  function(data) {
-					console.log(data);
-					$('h2').text(data[0].word);
 
-					$.each(data, function(i, definition){
-						$('.left').append('<span class="def">'+ definition.text + '</span>');					
-						console.log($(this));
-					}); // end each loop
+					if(data[0] === undefined) {
+						$('h2').text("Sorry, can't find " + input);
+					} else {
+						$('h2').text(data[0].word);
+						$.each(data, function(i, definition){
+							$('.left').append('<span class="def">'+ definition.text + '</span>');					
+						}); // end each loop
+					} //end if statement
 				} //end callback
 			} // end settings
+		); //end ajax
+
+		$.ajax(
+			'http://api.wordnik.com/v4/word.json/'+ input +'/pronunciations',
+			{
+				data : {
+					word: input,
+					api_key: 'dd938175caa9ac3a0a32c09fcc607d8638f0a0e82c8a0bbad'
+				}, //end data
+				success: function(data) {
+					$('.pronounce').html(data[0].raw);
+				} //end callback
+			} //end settings
 		); //end ajax
 
 		$.ajax(
@@ -44,43 +58,39 @@ $(document).ready(function(){
 					api_key: 'dd938175caa9ac3a0a32c09fcc607d8638f0a0e82c8a0bbad'
 				}, //end data
 				success: function(data) {
-					console.log(data);
-
 					$.each(data, function(i, related) {
 						var type = related.relationshipType;
 						$('.center').append('<h4>'+type + '</h4>');
 						$.each(related.words, function(i, word){
 							$('.center').append(word + '<br>');
 						});
-						}
-					);
-
-				}
+						} //end inner each
+					); //end outer each
+				} //end callback
 			} //end settings
 		); //end ajax
 
 		$.ajax(
-			'http://api.wordnik.com/v4/word.json/'+ input +'/topExample',
+			'http://api.wordnik.com/v4/word.json/'+ input +'/examples',
 			{
 				data : {
 					word: input,
+					limit: 200,
 					api_key: 'dd938175caa9ac3a0a32c09fcc607d8638f0a0e82c8a0bbad'
 				}, //end data
 				success: function(data) {
-					console.log(data);
-
-					$('.examples').html('<em>'+ data.text + '<br>' + '-' + data.title)+'</em>';
-
-				}
+					$.each(data.examples, function(i, example){
+						$('.right').append('<span class="ex">'+ example.text + '</span>');					
+					}); // end each loop
+				} //end callback
 			} //end settings
 		); //end ajax
 
-
+		$('#search').val('');
 		// getDefinition(input);
 		// getRelated(input);
 
 	});
-
 
 	// =========================================
 	// Use enter key
